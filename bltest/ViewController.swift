1752175e4e627f7cede40c8c97f2f348cae86b6d
Rian Sanjaya <rian.sj@gmail.com>
2019-02-04 19:58:19 +0700

    Initial Commit

---
+++ bltest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bltest
+//
+//  Created by Rian Sanjaya Ng on 04/02/19.
+//  Copyright © 2019 Rian Sanjaya Ng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
